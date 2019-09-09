ea8f0b692ee886deb3e3273e37dedf0600f6440a
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2019-09-09 11:45:03 -0400

    Base conversion done

--- all-your-base/Base.hs
+++ all-your-base/Base.hs
@@ -12,9 +12,23 @@ rebase inputBase outputBase inputDigits
 
 validBases :: Integral a => a -> a -> [a] -> Either (Error a) [a]
 validBases inBase outBase digits = 
-    let (validDigits,invalidDigits) = partition (<inBase) digits
+    let (validDigits,invalidDigits) = partition (inRange inBase) digits
     in if null invalidDigits then Right $ allValid inBase outBase validDigits 
                              else Left $ InvalidDigit (head invalidDigits)
 
 allValid :: Integral a => a -> a -> [a] -> [a]
-allValid inBase outBase digits = [1,2,3]
+allValid inBase outBase digits = let inDecimalBase = sum $ toDecimal inBase digits
+                                 in fromDecimal outBase inDecimalBase []
+
+toDecimal :: Integral a => a-> [a] -> [a]
+toDecimal inBase digits = let powers = fmap (inBase^) [0..]
+                          in  zipWith (*) powers $ reverse digits 
+
+fromDecimal ::Integral a => a -> a -> [a] -> [a]
+fromDecimal outBase 0 acc = acc
+fromDecimal outBase num acc = let quo = num `div` outBase
+                                  rem = num `mod` outBase
+                              in fromDecimal outBase quo (rem:acc)
+
+inRange :: Integral a => a -> (a -> Bool)
+inRange inBase = and . sequenceA [(<inBase),(>=0)]
