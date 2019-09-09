7abc1e09093cdc874a2bdc301047f0b3d7870436
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2019-09-09 10:53:48 -0400

    Done checking for errors

--- all-your-base/Base.hs
+++ all-your-base/Base.hs
@@ -1,7 +1,20 @@
 module Base (Error(..), rebase) where
+import Data.List (partition)
 
 data Error a = InvalidInputBase | InvalidOutputBase | InvalidDigit a
     deriving (Show, Eq)
 
 rebase :: Integral a => a -> a -> [a] -> Either (Error a) [a]
-rebase inputBase outputBase inputDigits = error "You need to implement this function."
+rebase inputBase outputBase inputDigits
+        |inputBase < 2  = Left InvalidInputBase
+        |outputBase < 2 = Left InvalidOutputBase
+        |otherwise = validBases inputBase outputBase inputDigits
+
+validBases :: Integral a => a -> a -> [a] -> Either (Error a) [a]
+validBases inBase outBase digits = 
+    let (validDigits,invalidDigits) = partition (<inBase) digits
+    in if null invalidDigits then Right $ allValid inBase outBase validDigits 
+                             else Left $ InvalidDigit (head invalidDigits)
+
+allValid :: Integral a => a -> a -> [a] -> [a]
+allValid inBase outBase digits = [1,2,3]
