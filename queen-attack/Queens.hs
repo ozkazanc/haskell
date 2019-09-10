113cb19c0b66db6df90353f8aaf1eed77456649a
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2019-09-10 16:26:13 -0400

    Queens canAttack done

--- queen-attack/Queens.hs
+++ queen-attack/Queens.hs
@@ -1,7 +1,21 @@
 module Queens (boardString, canAttack) where
 
 boardString :: Maybe (Int, Int) -> Maybe (Int, Int) -> String
-boardString white black = error "You need to implement this function."
+boardString Nothing Nothing = emptyBoard
+boardString Nothing (bx,by) = emptyBoard
+boardString (wx,wy) Nothing = emptyBoard
+boardString (ws,wy) (bx,by) = emptyBoasrd
+
 
 canAttack :: (Int, Int) -> (Int, Int) -> Bool
-canAttack queenA queenB = error "You need to implement this function."
+canAttack (x1,y1) (x2,y2)
+            | x1 == x2 = True
+            | y1 == y2 = True
+            | abs (x1-x2) == abs (y1-y2) = True
+            | otherwise = False			
+
+emptyBoard :: [String]
+emptyBoard = lines . concat . replicate 8 $ "________\n"
+
+copyBoard :: [String] -> (Int, Int) -> Char -> [String]
+copyBoard board (x,y) c = foldl (foldl)
