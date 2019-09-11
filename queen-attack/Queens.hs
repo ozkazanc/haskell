97e006c565eb4bf676b122185f8cf14bc9eec625
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2019-09-11 12:57:31 -0400

    Queen attack done

--- queen-attack/Queens.hs
+++ queen-attack/Queens.hs
@@ -1,10 +1,10 @@
 module Queens (boardString, canAttack) where
 
-boardString :: Maybe (Int, Int) -> Maybe (Int, Int) -> String
-boardString Nothing Nothing = emptyBoard
-boardString Nothing (bx,by) = emptyBoard
-boardString (wx,wy) Nothing = emptyBoard
-boardString (ws,wy) (bx,by) = emptyBoasrd
+boardString :: Maybe(Int, Int) -> Maybe(Int, Int) -> String
+boardString wQueen bQueen =
+    let whiteBoard = placeQueen 'W' wQueen
+        blackBoard = placeQueen 'B' bQueen
+    in  unlines . chunksOf 8 $ zipWith zipFunc whiteBoard blackBoard
 
 
 canAttack :: (Int, Int) -> (Int, Int) -> Bool
@@ -12,10 +12,19 @@ canAttack (x1,y1) (x2,y2)
             | x1 == x2 = True
             | y1 == y2 = True
             | abs (x1-x2) == abs (y1-y2) = True
-            | otherwise = False			
+            | otherwise = False
 
-emptyBoard :: [String]
-emptyBoard = lines . concat . replicate 8 $ "________\n"
+zipFunc :: Char -> Char -> Char
+zipFunc ' ' b = b
+zipFunc w ' ' = w
+zipFunc _ _ = error "Both queens on the same square"
 
-copyBoard :: [String] -> (Int, Int) -> Char -> [String]
-copyBoard board (x,y) c = foldl (foldl)
+placeQueen :: Char -> Maybe(Int,Int) -> String
+placeQueen _ Nothing = placeQueen ' ' (Just(0,0))
+placeQueen c (Just(x,y)) = let index = x * 8 + y
+                     in replicate index ' ' ++ [c] ++ replicate (63 - index) ' '
+
+
+chunksOf :: Int -> [Char] -> [[Char]]
+chunksOf _ [] = []
+chunksOf n xs = (take n xs):chunksOf n (drop n xs)
